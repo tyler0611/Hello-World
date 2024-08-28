6f0844d47102556dbcd468c38fe4e64e280c40e8
tyler0611 <berlity0611@gmail.com>
2024-08-27 21:46:55 -0500

    new app

--- Hello World/ContentView.swift
+++ Hello World/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var color = Color.white
+    @State private var date = Date()
+    @State private var number = 0
+    @State private var celsius = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Group{
+                Text("Hello, world!")
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+                Label("Square Root", systemImage: "x.squareroot")
+                    .padding()
+                Button("Click Me"){
+                }
+                Image("BHS")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                Link("Barrington High School", destination: URL(string:"https://bhs.barrington220.org/")!)
+                    .padding()
+            }
+            Group{
+                ColorPicker("Pick a Color", selection: $color)
+                    .frame(width: 150, height: 50)
+                DatePicker("Pick a Date", selection: $date)
+                    .frame(width: 320, height: 50)
+                HStack {
+                    Text("Pick a Number")
+                    Picker("", selection: $number){
+                        Text("1").tag(1)
+                        Text("2").tag(2)
+                        Text("3").tag(3)
+                        Text("4").tag(4)
+                    }
+                    .pickerStyle(.segmented)
+                    .frame(width: 200, height: 100)
+                    .padding()
+                }
+                Slider(value: $celsius, in: -100...100)
+                Text("\(celsius, specifier: "%.1f") Celsius is \(celsius * 9 / 5 + 32, specifier: "%.1f") Fahrenheit")
+            }
         }
-        .padding()
+        .background(color)
     }
 }
 
